c0af913871b600e1667a101c8c190b12e34494f3
mateuszsikora <mateusz.sikora@nokia.com>
2015-12-17 15:51:48 +0100

    remove unnecessary imports

--- Icon/icon.ts
+++ Icon/icon.ts
@@ -1,8 +1,6 @@
 import {Component, EventEmitter} from 'angular2/core';
 import {CORE_DIRECTIVES} from 'angular2/common';
 
-import '../materialize';
-
 @Component({
   selector: Icon.toString(),
   directives: [CORE_DIRECTIVES],
