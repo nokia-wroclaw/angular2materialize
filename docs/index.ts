901f30b6d94f733d42c50bf420573a93495e8b85
mateuszsikora <mateusz.sikora@nokia.com>
2016-03-15 13:06:45 +0100

    typings instead of tsd

--- docs/index.ts
+++ docs/index.ts
@@ -1,4 +1,4 @@
-/// <reference path="./typings/custom.d.ts" />
+/// <reference path="../typings/custom.d.ts" />
 
 import {Component} from 'angular2/core';
 import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
